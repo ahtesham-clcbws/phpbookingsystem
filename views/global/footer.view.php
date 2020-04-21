247aace8215cdd4d13e545abc907944dd06e7859
ahtesham2016 <ahtesham2000@gmail.com>
2020-04-21 15:15:03 -0700

    Login modal and other changes

--- views/global/footer.view.php
+++ views/global/footer.view.php
@@ -1,7 +1,30 @@
+<div id="footer" class="ion-padding">
+	<ion-grid>
+		<ion-row>
+			<ion-col>
+				<div class="ion-text-start">
+					<ion-text color="dark">
+						<h2>Stay informed</h2>
+					</ion-text>
+				</div>
+
+			</ion-col>
+		</ion-row>
+		<ion-row>
+			<ion-col></ion-col>
+			<ion-col></ion-col>
+			<ion-col></ion-col>
+			<ion-col></ion-col>
+		</ion-row>
+	</ion-grid>
+</div>
+
+
 </main>
+
 <ion-footer>
-	<ion-toolbar class="ion-padding-vertical">
-		<ion-title>Footer</ion-title>
+	<ion-toolbar class="ion-padding-horizontal">
+		<ion-text>© 2020 Company Name, Inc. All rights reserved· Privacy Terms Sitemap</ion-text>
 	</ion-toolbar>
 </ion-footer>
 </ion-content>
